5f5598fece92b151133e55fc3c90f2d1a789b5c0
rebeccavimal <rebeccavimalraj@gmail.com>
2024-06-14 14:49:06 +0530

    Add files via upload

---
+++ Test Suites/udc/All Pages.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All Pages</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8f61ad60-2e19-49a3-a225-d211711573d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>246cc3b5-a661-4df4-b9ec-66977560c7a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/udc/All Pages</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
